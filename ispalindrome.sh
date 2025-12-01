bffb8338260dd2599e8adeb8f923de0648289ef1
Ruturaj Rawate <ruturawate611@gmail.com>
2025-12-02 03:54:37 +0530

    Added new files

---
+++ ispalindrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -n "Enter a string: "
+read str
+
+rev=$(echo "$str" | rev)
+
+if [ "$str" == "$rev" ]; then
+    echo "\"$str\" is a palindrome."
+else
+    echo "\"$str\" is not a palindrome."
+fi
---
+++ isprime.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo -n "Enter a number: "
+read num
+
+if [ $num -lt 2 ]; then
+    echo "$num is not a prime number."
+    exit
+fi
+
+is_prime=1
+
+for ((i=2; i*i<=num; i++))
+do
+    if (( num % i == 0 )); then
+        is_prime=0
+        break
+    fi
+done
+
+if [ $is_prime -eq 1 ]; then
+    echo "$num is a prime number."
+else
+    echo "$num is not a prime number."
+fi
